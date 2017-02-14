b8544f9c98000e31d28ed22c2a6a3d1d16891860
My mongo <mymongo78@gmail.com>
2017-02-14 22:47:50 +0900

    create generateMessage utility

--- public/js/index.js
+++ public/js/index.js
@@ -2,24 +2,10 @@
 
         socket.on('connect', function () {
             console.log('Connected to server');
-
-            socket.emit('createEmail',{
-                to:'jen@example.com',
-                text:'Hey. This is Andrew.'
-            });
-
-            socket.emit('createMessage',{
-                from:'me',
-                text:'Hello.... '
-            });
         });
 
         socket.on('disconnect',function (){
             console.log('Disconnected from server');
-        });
-
-        socket.on('newEmail',function (email) {
-            console.log('New email',email);
         });
 
         socket.on('newMessage', function (message){
--- server/server.js
+++ server/server.js
@@ -3,43 +3,34 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname,'../public');
+const {generateMessage} = require('./utils/message');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log('New user connected');
-    socket.emit('newEmail',{
-        from:'mike@example.com',
-        text:'Hey. What is going on.',
-        createdAt:123
-    });
 
-    socket.on('createEmail',(newEmail)=>{
-        console.log('createEmail',newEmail);
-    });
+    socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
 
-    socket.emit('newMessage',{
-        from:'andrew',
-        text:'Hello',
-        createdAt:'123'
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined'));
 
-    socket.on('createMessage',(message)=>{
-        console.log('createMessage',message);
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', generateMessage(message.from,message.text));
     });
 
 
-    socket.on('disconnect',()=>{
+    socket.on('disconnect', () => {
         console.log('Disconnected from client');
     })
 });
 
 app.use(express.static(publicPath));
 
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log(`Server on port ${port}`);
 });
