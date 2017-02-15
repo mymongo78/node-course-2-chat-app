c7218de7871660f676f8aa878e727787c960d757
My mongo <mymongo78@gmail.com>
2017-02-15 23:46:18 +0900

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -26,11 +26,12 @@
 
         jQuery('#message-form').on('submit',function (e) {
             e.preventDefault();
+            var messageTextbox = jQuery('[name=message]');
             socket.emit('createMessage',{
                 from:'User',
-                text:jQuery('[name=message]').val()
+                text: messageTextbox.val()
             },function(){
-
+                messageTextbox.val('');
             });
         });
 
@@ -39,12 +40,15 @@
             if (!navigator.geolocation) {
                 return alert('Geolocation not supported by your browser.');
             }
+            locationButton.attr('disabled','disabled').text('Sending...');
             navigator.geolocation.getCurrentPosition(function(position){
+                locationButton.removeAttr('disabled').text('Send Location');
                 socket.emit('createLocationMessage',{
                     latitude: position.coords.latitude,
                     longitude: position.coords.longitude
                 });
             },function(){
+                locationButton.removeAttr('disabled').text('Send Location');
                 alert('Unable to fetch location');
             })
         });
