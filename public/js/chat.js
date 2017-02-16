8105947b85d00ab0e1d1754877de968195c0f813
My mongo <mymongo78@gmail.com>
2017-02-16 20:54:24 +0900

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -61,7 +61,7 @@ socket.on('newLocationMessage', function (message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
-    scollToBottom();
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
