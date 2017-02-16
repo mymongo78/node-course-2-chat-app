0804b6daafc61bd8c747fbb7e7953d77013b6d03
My mongo <mymongo78@gmail.com>
2017-02-16 10:50:29 +0900

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
         var socket = io();
 
+        function scrollToBottom(){
+            //Selectors
+            var messages = jQuery('#messages');
+            var newMessage = messages.children('li:last-child');
+            //Heights
+            var clientHeight = messages.prop('clientHeight');
+            var scrollTop = messages.prop('scrollTop');
+            var scrollHeight = messages.prop('scrollHeight');
+            var newMessageHeight = newMessage.innerHeight();
+            var lastMessageHeight = newMessage.prev().innerHeight();
+
+            if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+                messages.scrollTop(scrollHeight);
+            }
+        }
+
         socket.on('connect', function () {
             console.log('Connected to server');
         });
@@ -17,6 +33,7 @@
                 createdAt:formattedTime
             });
             jQuery('#messages').append(html);
+            scrollToBottom();
         });
 
         socket.on('newLocationMessage',function (message) {
@@ -28,6 +45,7 @@
                 createdAt:formattedTime
             });
             jQuery('#messages').append(html);
+            scollToBottom();
         });
 
         jQuery('#message-form').on('submit',function (e) {
